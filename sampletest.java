18f0402f7e1e78dec887d4e95b6fb0f0a0adb014
sarve <sarvesh.c.sinha@gmil.com>
2018-09-12 21:17:43 -0500

    sample commit

---
+++ sampletest.java
@@ -0,0 +1,16 @@
+public class sampletest {
+
+
+    public sampletest() {
+    }
+
+    Sample2Test stest = new Sample2Test();
+    public boolean isletter =false;
+
+//      
+
+    public void setDigit(boolean digit) {
+        isletter = digit;
+
+    }
+}
